027b495f1880dc1648fd142afedd0ce96275a7eb
Matheus Mazepa <matheusmazepa@Matheuss-MacBook-Pro.local>
2018-02-08 17:41:11 -0200

    Troca nome vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,4 @@
-import Vuex from 'Vuex';
+import Vuex from 'vuex';
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
